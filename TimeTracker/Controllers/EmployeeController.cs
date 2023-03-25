f256fef49b694e55cb8ebe837e97753f56edb5f4
AmmarShaalan <ammar.shaalan@dsquares.com>
2023-03-25 03:45:10 +0200

    Add project files.

---
+++ TimeTracker/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System;
+
+namespace TimeTracker
+{
+    public class EmployeeController : Controller
+    {
+        private static HttpClient _client = new HttpClient();
+        private const string _apiUrl = "https://rc-vault-fap-live-1.azurewebsites.net/api/gettimeentries?code=vO17RnE8vuzXzPJo5eaLLjXjmRW07law99QTD90zat9FfOQJKKUcgQ==";
+
+        // GET: Employee
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                var response = await _client.GetAsync(_apiUrl);
+                response.EnsureSuccessStatusCode();
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+
+                var employees = JsonConvert.DeserializeObject<List<TimeEntries>>(responseBody)
+                                           .Where(te => !string.IsNullOrEmpty(te.EmployeeName))
+                                           .GroupBy(te => te.EmployeeName)
+                                           .Select(g => new EmployeeViewModel
+                                           {
+                                               Name = g.Key,
+                                               TotalTimeWorked = g.Sum(te => (te.EndTimeUtc - te.StarTimeUtc).TotalHours)
+                                           })
+                                           .OrderByDescending(vm => vm.TotalTimeWorked)
+                                           .ToList();
+
+                return View(employees);
+            }
+            catch(Exception ex)
+            {
+                var errorMessage = "An error occurred while retrieving data from the API: " + ex.Message;
+                return View("Error", new { message = errorMessage });
+            }
+        }
+    }
+}
---
+++ TimeTracker/Models/EmployeeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TimeTracker   
+{
+    public class EmployeeViewModel
+    {
+        public string Name { get; set; }
+        public double TotalTimeWorked
+        {
+            get { return _totalTimeWorked; }
+            set { _totalTimeWorked = Math.Round(value, 2); }
+        }
+
+        private double _totalTimeWorked;
+    }
+}
